1ee30c22610b325c90e24ed32b0ba148eaf6328c
Eric Andres <emandres@gmail.com>
2017-08-30 11:28:19 -0600

    Remove functionality for presentation

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,6 @@
 import {Observable, Observer, Subject} from 'rxjs'
-import {repeat} from 'rxjs/operator/repeat'
 
 type Color = 'red' | 'green' | 'purple'
-
-let observer: Observer<Color>
-let observable: Observable<Color>
-
-observable = Observable.create(_ => {})
-let subject = new Subject()
-observable.subscribe(subject)
 
 const fireLaser = (color) => {
     let space = document.getElementById('space')
@@ -25,19 +17,3 @@ const updateLaserCounter = ([color, count]) => {
         counters.item(i).innerHTML = `${count}`
     }
 }
-
-var buttons = document.getElementsByClassName('laser-button')
-for (let i = 0; i < buttons.length; i++) {
-    buttons.item(i).addEventListener('click', e => {
-        subject.next((<any>e.target).dataset.color)
-    })
-}
-
-subject
-    .debounceTime(100)
-    .subscribe(fireLaser)
-
-subject
-    .groupBy(x => x)
-    .flatMap(x => x.scan((acc: [Color, number], _) => [x.key, acc[1] + 1], [x.key, 0]))
-    .subscribe(updateLaserCounter)
